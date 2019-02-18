d57954f2b46c023e53d76b8b0e0e7d96d1155cd0
yangyang01 <yangyang01@iyunxiao.com>
2019-02-18 20:06:15 +0800

    这是我配置nginx时，写的一个练习项目

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require("express");
 let app = express();
 
 app.all('*',function(req,res) {
-    res.send("I am one project ...");
+    res.send("这是我配置nginx时，写的一个练习项目");
 });
 app.listen(8081);
